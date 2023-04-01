dfa02a10903ea95722c699c148041092578d68f0
Niyaz <nmingaripov@gmail.com>
2023-04-01 10:35:50 +0300

    exception handling, memory freeing up

--- main.cpp
+++ main.cpp
@@ -3,31 +3,48 @@
 
 int main() {
 	// Whole PC
-	PersonalComputer* pc = new PersonalComputer("InkPC");
+	auto pc = new PersonalComputer("InkPC");
 
 	// PC components
-	ComputerCase* comp_case = new ComputerCase("Corsair 4000D");
-	Monitor* monitor = new Monitor("ASUS TUF Gaming");
-	Keyboard* keyboard = new Keyboard("Smartbuy SBK-610");
-	Mouse* mouse = new Mouse("Reddragon Impact");
+	auto comp_case = new ComputerCase("Corsair 4000D");
+	auto monitor = new Monitor("ASUS TUF Gaming");
+	auto keyboard = new Keyboard("Smartbuy SBK-610");
+	auto mouse = new Mouse("Reddragon Impact");
 	pc->add(comp_case);
 	pc->add(monitor);
 	pc->add(keyboard);
 	pc->add(mouse);
 
 	// Computer case components
-	MotherBoard* board = new MotherBoard("ASUS PRIME B450");
-	PowerSupply* supply = new PowerSupply("1000W Phanteks");
+	auto board = new MotherBoard("ASUS PRIME B450");
+	auto supply = new PowerSupply("1000W Phanteks");
 	comp_case->add(board);
 	comp_case->add(supply);
 
 	// Mother board components
-	RAM* ram = new RAM("Corsair 64 GB");
-	GPU* gpu = new GPU("NVIDIA RTX 4090");
-	CPU* cpu = new CPU("AMD Ryzen 9 7950X");
+	auto ram = new RAM("Corsair 64 GB");
+	auto gpu = new GPU("NVIDIA RTX 4090");
+	auto cpu = new CPU("AMD Ryzen 9 7950X");
 	board->add(ram);
 	board->add(cpu);
 	board->add(gpu);
 
-	std::cout << "Hello, CMake!" << "\n";
+	/*
+	* It is impossible to add a component to a unit component, but the possibility
+	* of calling such a method exists for the sameness of the interface.
+	*/
+	try {
+		cpu->add(gpu);
+	}
+	catch (const std::exception& e) {
+		std::cout << "Exception: " << e.what() << "\n";
+	}
+
+	std::cout << "The PC is built successfully!" << "\n";
+
+	/*
+	* Deleting the root of a "tree" --> ñauses chain deletion of all components 
+	* (with destructor of BaseCompositeComponents class is correct.
+	*/
+	delete pc;
 }
